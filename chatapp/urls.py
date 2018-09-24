1b2489a21b4ba18f0969e0e071f678d09a8dbf82
biseshbhattarai <aashishbhattarai49@gmail.com>
2018-09-24 18:28:50 +0545

    Authentication rebuilt

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',  auth_views.LoginView.as_view(), {'template_name': 'chatapp/login.html'}, name="login"),
+    path('logout/',  auth_views.LogoutView.as_view(), {'template_name': 'chatapp/logout.html'}, name="login"),
+
     path('chat/', views.index, name="index"),
     re_path(r'^chat/(?P<room_name>[^/]+)/$', views.room, name='room')
 ]
