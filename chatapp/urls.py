52b93bcbc27643c13a2b9b2088a56944bba33d05
biseshbhattarai <aashishbhattarai49@gmail.com>
2018-09-24 17:53:59 +0545

    Login feature added

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('',  auth_views.LoginView.as_view(), {'template_name': 'chatapp/login.html'}, name="login"),
     path('chat/', views.index, name="index"),
     re_path(r'^chat/(?P<room_name>[^/]+)/$', views.room, name='room')
 ]
