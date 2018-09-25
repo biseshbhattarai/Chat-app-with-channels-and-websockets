47e774bd9f4c4a87e8541bd78c7cbbd33f1e1b94
biseshbhattarai <aashishbhattarai49@gmail.com>
2018-09-25 21:06:46 +0545

    Registration added

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 import json
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 @login_required
 def index(request):
@@ -12,3 +13,16 @@ def room(request, room_name):
     return render(request, 'chatapp/chatroom.html', {
         'room_name_json' : mark_safe(json.dumps(room_name))
     })
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            save = form.save(commit=False)
+            password = form.cleaned_data['password']
+            save.set_password(password)
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'chatapp/register.html', {'form':form})
